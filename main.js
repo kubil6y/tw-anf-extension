38a88929c48b136a941ff61b14894a64e80bf423
Kubilay Humm <lieqb2@gmail.com>
2021-06-25 21:04:59 +0300

    improved

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const KEY = 'video-muted';
-const videoMutedSettings = JSON.parse(localStorage.getItem(KEY));
-const newVideoMutedSettings = JSON.stringify({
-  ...videoMutedSettings,
-  carousel: true,
-});
-
-localStorage.setItem(KEY, newVideoMutedSettings);
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+const main = async () => {
+  const KEY = 'video-muted';
+  const videoMutedSettings = JSON.parse(localStorage.getItem(KEY));
+  const newVideoMutedSettings = JSON.stringify({
+    ...videoMutedSettings,
+    carousel: true,
+  });
+  localStorage.setItem(KEY, newVideoMutedSettings);
+  console.log('*** VIDEO HAS BEEN MUTED ***');
+  await sleep(10000);
+  const vid = document.querySelector('video');
+  if (vid) {
+    vid.pause();
+    console.log('*** VIDEO HAS BEEN PAUSED ***');
+  }
+};
+main();
