75684cbe0471c6adfd608b05cca971b27ec089de
Kubilay Humm <lieqb2@gmail.com>
2021-06-25 21:48:10 +0300

    refactor

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 const main = async () => {
-  const KEY = 'video-muted';
-  const videoMutedSettings = JSON.parse(localStorage.getItem(KEY));
-  const newVideoMutedSettings = JSON.stringify({
-    ...videoMutedSettings,
-    carousel: true,
-  });
-  localStorage.setItem(KEY, newVideoMutedSettings);
-  console.log('*** VIDEO HAS BEEN MUTED ***');
-  await sleep(10000);
-  const vid = document.querySelector('video');
-  if (vid) {
-    vid.pause();
-    console.log('*** VIDEO HAS BEEN PAUSED ***');
+  try {
+    const KEY = 'video-muted';
+    const videoMutedSettings = JSON.parse(localStorage.getItem(KEY));
+    const newVideoMutedSettings = JSON.stringify({
+      ...videoMutedSettings,
+      carousel: true,
+    });
+    localStorage.setItem(KEY, newVideoMutedSettings);
+    if (
+      videoMutedSettings.hasOwnProperty('carousel') &&
+      !videoMutedSettings['carousel']
+    ) {
+      console.log('*** VIDEO HAS BEEN MUTED ***');
+    }
+    await sleep(10000);
+    const vid = document.querySelector('video');
+    if (vid) {
+      vid.pause();
+      console.log('*** VIDEO HAS BEEN PAUSED ***');
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
 main();
